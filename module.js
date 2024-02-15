fe952627d8d128533fd88fff8c8d1c441506c4af
devfabien <ish001fabi@gmail.com>
2024-02-15 11:44:05 +0200

    feat: working with modules and require

---
+++ module.js
@@ -0,0 +1,7 @@
+const { people, ages } = require("./people");
+
+console.log(people, ages);
+
+// This will help you to get the information of the current operating system.
+const os = require("os");
+console.log(os.platform(), os.homedir());
---
+++ people.js
@@ -0,0 +1,5 @@
+const people = ["Junior", "Fabien", "Cynthia"];
+const ages = [22, 19, 26];
+console.log(people);
+
+module.exports = { people, ages };
