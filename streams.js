ac5954dae43c2b1aa004900cad8cdc11a0b5d9a6
devfabien <ish001fabi@gmail.com>
2024-02-16 15:32:19 +0200

    refactor: encoding the buffer into a readable format

--- streams.js
+++ streams.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 
-const readStream = fs.createReadStream("./docs/blog2.txt");
+const readStream = fs.createReadStream("./docs/blog2.txt", {
+  encoding: "utf8",
+});
 
 readStream.on("data", (chunk) => {
   console.log("-----new chunk---------");
