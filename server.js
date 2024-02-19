8a859ae4f597eaf0ddc9b4b9df3ce3d0c5f2c80a
devfabien <ish001fabi@gmail.com>
2024-02-19 08:17:35 +0200

    feat: setting the response as a plain text

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  // Set header content type
+  res.setHeader("Content-type", "text/plain");
+  res.write("Hello, Devfabien");
+  res.end();
 });
 
 server.listen(3007, "localhost", () => {
