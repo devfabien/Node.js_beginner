cccb086726bb3a045c3f8543ff40b42e3b706e19
devfabien <ish001fabi@gmail.com>
2024-02-19 09:10:14 +0200

    feat: adding a redirect to about page

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ const server = http.createServer((req, res) => {
       path += "about.html";
       res.statusCode = 200;
       break;
+    case "/about-me":
+      res.setHeader("Location", "/about");
+      res.statusCode = 301;
+      break;
     default:
       path += "404.html";
       res.statusCode = 404;
