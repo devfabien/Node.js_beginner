d886a6d2bb9df5576adaee7a98563d5e6f575530
devfabien <ish001fabi@gmail.com>
2024-02-19 09:09:44 +0200

    feat: adding status codes

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const http = require("http");
 const fs = require("fs");
-
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
@@ -11,12 +10,15 @@ const server = http.createServer((req, res) => {
   switch (req.url) {
     case "/":
       path += "index.html";
+      res.statusCode = 200;
       break;
     case "/about":
       path += "about.html";
+      res.statusCode = 200;
       break;
     default:
       path += "404.html";
+      res.statusCode = 404;
       break;
   }
 
