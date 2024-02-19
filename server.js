6d3479f5c6b16f4835962bb22419e1e44bd52bba
devfabien <ish001fabi@gmail.com>
2024-02-19 08:25:21 +0200

    feat: sending an html file as a response

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
   // Set header content type
   res.setHeader("Content-type", "text/html");
-  res.write("<h1>Hello, Devfabien</h1>");
-  res.write("<p>A passionate developer</p>");
-  res.end();
+
+  // Send an Html file
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(3007, "localhost", () => {
