a7ed65a281d3e68fe8153fc39122a5c93528717a
devfabien <ish001fabi@gmail.com>
2024-02-20 07:58:04 +0200

    feat: creating the routes using express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+app.listen(3007);
