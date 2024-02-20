586bc28f7c265fa116d51dec9ac52dd26ecc518d
devfabien <ish001fabi@gmail.com>
2024-02-20 16:56:06 +0200

    refactor: rendering the pages using ejs

--- app.js
+++ app.js
@@ -2,22 +2,29 @@ const express = require("express");
 
 const app = express();
 
+// register view engine
+
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
 // redirects
 app.get("/about-me", (req, res) => {
   res.redirect("/about");
 });
+app.get("/newBlog", (req, res) => {
+  res.render("create");
+});
 
 // 404
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
 
 app.listen(3007);
